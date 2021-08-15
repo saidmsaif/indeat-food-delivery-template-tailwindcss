47c948220ad8830ee338e84918c10278b661ee29
saidmsaif <saidmsaiff@gmail.com>
2021-08-15 12:35:15 +0600

    header design and add assets files

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,25 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.indeat-container': {
+          maxWidth: '1440px',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+        }
+      })
+    }
+  ]
 }
